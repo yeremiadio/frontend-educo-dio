1daba04fd5e32001f66ff8eda9a06b0e9197e972
diwa1927 <hadiwardana90@gmail.com>
2023-08-09 11:58:46 +0700

    commits

--- src/page/profile.js
+++ src/page/profile.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import BottomAppBar from "../components/Navbar/BottomAppBar";
+import { Avatar, Card, CardContent, CardHeader, Divider, Typography } from "@mui/material";
 
 const Profile = () => {
     const { user: currentUser } = useSelector((state) => state.auth);
@@ -13,22 +14,38 @@ const Profile = () => {
     return (
         <div className="main">
             <div className="content">
-                <header>
-                    <h3>
-                    <strong>{currentUser.username}</strong> Profile
-                    </h3>
-                </header>
-                <p>
-                    <strong>Id:</strong> {currentUser.id}
-                </p>
-                <p>
-                    <strong>Email:</strong> {currentUser.email}
-                </p>
-                <strong>Authorities:</strong>
-                <ul>
-                    {currentUser.roles &&
-                    currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-                </ul>
+                <Card sx={{ bgcolor: "transparent", color: "white" }}>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ bgcolor: "darkblue" }} aria-label="Ava" >{currentUser.id}</Avatar>
+                        }
+                        title={
+                            <Typography variant="h5" >
+                                <strong>{currentUser.username} Profile.</strong>
+                            </Typography>
+                        }
+                        subheader={currentUser.createAt}
+                        titleTypographyProps={{ align: "center" }}
+                    />
+                    <CardContent>
+                        <Typography component={"h5"} variant="h6" sx={{ alignContent: "center"}} >
+                            <strong>ID : {currentUser.id} </strong>
+                        </Typography>
+                        <Typography component={"h5"} variant="h6" sx={{ alignContent: "center"}} >
+                            <strong>Email : {currentUser.email} </strong>
+                        </Typography>
+                        <Divider/>
+                        <Typography component={"h5"} variant="h6" sx={{ alignContent: "center"}} >
+                            <strong>Authorities : </strong>
+                        </Typography>
+                        <Typography component={"h5"} variant="h6" sx={{ alignContent: "center"}} >
+                            <strong> 
+                                {currentUser.roles &&
+                                currentUser.roles.map((role, index) => <li key={index}>{role}</li>)} 
+                            </strong>
+                        </Typography>
+                    </CardContent>
+                </Card>
             </div>
             <BottomAppBar/>
         </div>
