51be27da9cb71e1a455c21af08bbeacdd500725e
diwa1927 <hadiwardana90@gmail.com>
2023-11-15 17:35:15 +0700

    Dashboard dan Materi

--- src/page/dashboard/Dashboard.js
+++ src/page/dashboard/Dashboard.js
@@ -1,58 +1,95 @@
 import { Card, CardContent, CardHeader, CardMedia, Container, Divider, Grid, ThemeProvider, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { theme } from "../../utils/ThemeProvider";
 import { WavingHand } from '@mui/icons-material';
 import TopAppBar from '../../components/Navbar/TopAppBar';
 import { dataDashboard } from '../../utils/DataDashboard';
 import BottomAppBar from '../../components/Navbar/BottomAppBar';
 import DashboardAssignments from '../../components/Table/DashboardAssignments';
+import { Navigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import axiosInstance from '../../config/axiosInstance';
 
 export default function Dashboard() {
+  const [userInfo, setUserInfo] = useState(null);
+    // Menggunakan localStorage untuk mendapatkan token akses
+    const accessToken = localStorage.getItem('accessToken');
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                if (accessToken) {
+                    // Mendekode token akses untuk mendapatkan informasi pengguna
+                    const decodedToken = jwtDecode(accessToken);
+                    const userId = decodedToken.id;
+
+                    // Mendapatkan informasi pengguna berdasarkan ID pengguna yang login
+                    const response = await axiosInstance.get(`/api/users/${userId}`, {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                        },
+                    });
+                    
+                    setUserInfo(response.data.user);
+                }
+            } catch (error) {
+                console.error('Error fetching user information:', error);
+            }
+        };
+
+        fetchUserInfo();
+    }, [accessToken]);
+
+    if (!accessToken) {
+        return <Navigate to="/login" />;
+    }
 
   return (
     <ThemeProvider theme={theme}>
       <TopAppBar/>
-      <div className='content'>
-      <Container component={"main"} maxWidth="md" sx={{ pt: 5, pb: 6 }}>
-        <Typography align='center' variant='h3' sx={{fontStyle: "inherit", fontFamily: "initial", fontWeight: 30}} gutterBottom>
-          Hallo Codingers <WavingHand sx={{fontSize: 50}}/>
-        </Typography>
-        <Typography align='center' variant='h6' sx={{color: "white", fontFamily: "revert"}} gutterBottom>
-          {
-          "Welcome to Your Dashboard, How are you today?"
-          }
-        </Typography>
-      </Container>
-      <Divider component="animate" sx={{color: "white", fontSize: 10,}} />
-      <Container component={"main"} maxWidth="md" sx={{margin: 2,}} >
-        <Grid container spacing={5} alignItems="center">
-          {dataDashboard.map((item, index) => {
-            return (
-              <Grid item key={index} lg={4} md={4} sm={4} xs={12}>
-                <Card variant='outlined' >
-                  <CardHeader
-                    title={item.title}
-                    titleTypographyProps={{ align: "center" }}
-                    sx={{fontStyle: "initial", fontFamily: "monospace"}}
-                  />
-                  <CardMedia
-                    component="img"
-                    image={item.image}
-                    alt={item.title}
-                  />
-                  <CardContent>
-                  <Typography>{item.description}</Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
-      <Container component={"main"} maxWidth="md" sx={{margin: 2,}}>
-          <DashboardAssignments/>
-      </Container>
-      </div>
+      {userInfo && (
+        <div className='content'>
+        <Container component={"main"} maxWidth="md" sx={{ pt: 5, pb: 6 }}>
+          <Typography align='center' variant='h3' sx={{fontStyle: "inherit", fontFamily: "initial", fontWeight: 30}} gutterBottom>
+            Hallo Codingers <WavingHand sx={{fontSize: 50}}/>
+          </Typography>
+          <Typography align='center' variant='h6' sx={{color: "white", fontFamily: "revert"}} gutterBottom>
+            {
+            "Welcome to Your Dashboard, How are you today?"
+            }
+          </Typography>
+        </Container>
+        <Divider component="animate" sx={{color: "white", fontSize: 10,}} />
+        <Container component={"main"} maxWidth="md" sx={{margin: 2,}} >
+          <Grid container spacing={5} alignItems="center">
+            {dataDashboard.map((item, index) => {
+              return (
+                <Grid item key={index} lg={4} md={4} sm={4} xs={12}>
+                  <Card variant='outlined' >
+                    <CardHeader
+                      title={item.title}
+                      titleTypographyProps={{ align: "center" }}
+                      sx={{fontStyle: "initial", fontFamily: "monospace"}}
+                    />
+                    <CardMedia
+                      component="img"
+                      image={item.image}
+                      alt={item.title}
+                    />
+                    <CardContent>
+                    <Typography>{item.description}</Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+        <Container component={"main"} maxWidth="md" sx={{margin: 2,}}>
+            <DashboardAssignments/>
+        </Container>
+        </div>
+      )}
       <BottomAppBar/>
     </ThemeProvider>
   )
--- src/page/materi/Materi.js
+++ src/page/materi/Materi.js
@@ -4,35 +4,75 @@ import TopAppBar from '../../components/Navbar/TopAppBar'
 import { theme } from "../../utils/ThemeProvider";
 import MateriContent from './MateriContent';
 import BottomAppBar from '../../components/Navbar/BottomAppBar';
+import { Navigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import axiosInstance from '../../config/axiosInstance';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Materi() {
+    const [userInfo, setUserInfo] = useState(null);
+    // Menggunakan localStorage untuk mendapatkan token akses
+    const accessToken = localStorage.getItem('accessToken');
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                if (accessToken) {
+                    // Mendekode token akses untuk mendapatkan informasi pengguna
+                    const decodedToken = jwtDecode(accessToken);
+                    const userId = decodedToken.id;
+
+                    // Mendapatkan informasi pengguna berdasarkan ID pengguna yang login
+                    const response = await axiosInstance.get(`/api/users/${userId}`, {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                        },
+                    });
+                    
+                    setUserInfo(response.data.user);
+                }
+            } catch (error) {
+                console.error('Error fetching user information:', error);
+            }
+        };
+
+        fetchUserInfo();
+    }, [accessToken]);
+
+    if (!accessToken) {
+        return <Navigate to="/login" />;
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <TopAppBar/>
-            <div className='content'>
-            <Container component={"main"} maxWidth="lg"  sx={{ pt: 5, pb: 6, alignItems: "center" }}>
-                <Typography align='center' variant='h3' sx={{ color:"white", fontStyle: "inherit", fontFamily: "initial", fontWeight: 30}} gutterBottom>
-                    Materi's Page 
+            {userInfo && (
+                <div className='content'>
+                <Container component={"main"} maxWidth="lg"  sx={{ pt: 5, pb: 6, alignItems: "center" }}>
+                    <Typography align='center' variant='h3' sx={{ color:"white", fontStyle: "inherit", fontFamily: "initial", fontWeight: 30}} gutterBottom>
+                        Materi's Page 
+                        </Typography>
+                        <Typography align='center' variant='h6' sx={{color: "white", fontFamily: "revert"}} gutterBottom>
+                        {
+                        "Step by step, I will guide you to understand what you want to comprehend."
+                        }
                     </Typography>
-                    <Typography align='center' variant='h6' sx={{color: "white", fontFamily: "revert"}} gutterBottom>
-                    {
-                    "Step by step, I will guide you to understand what you want to comprehend."
-                    }
-                </Typography>
-                <br/>
-                <Divider />
-            </Container>
-            <Container component={"main"} maxWidth="lg" sx={{margin: 2}}>
-                <MateriContent
-                    item1 = "Variabel & Tipe Data"
-                    item2 = "Operator"
-                    item3 = "Percabangan"
-                    item4 = "Perulangan"
-                    item5 = "Array"
-                    item6 = "Full Tutorial"
-                />
-            </Container>
-            </div>
+                    <br/>
+                    <Divider />
+                </Container>
+                <Container component={"main"} maxWidth="lg" sx={{margin: 2}}>
+                    <MateriContent
+                        item1 = "Variabel & Tipe Data"
+                        item2 = "Operator"
+                        item3 = "Percabangan"
+                        item4 = "Perulangan"
+                        item5 = "Array"
+                        item6 = "Full Tutorial"
+                    />
+                </Container>
+                </div>
+            )}
             <BottomAppBar/>
         </ThemeProvider>
     )
