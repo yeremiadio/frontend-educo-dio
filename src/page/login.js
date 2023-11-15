186892324e4a1f4e8537cb7b60d1796fca3015ae
diwa1927 <hadiwardana90@gmail.com>
2023-11-15 11:47:36 +0700

    cek login

--- src/page/login.js
+++ src/page/login.js
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
 import { Avatar, Button, Container, TextField, ThemeProvider, Typography } from "@mui/material";
-import { login } from "../utils/slices/auth";
-import { clearMessage } from "../utils/slices/message";
 import { theme } from "../utils/ThemeProvider";
 import { Lock, Visibility, VisibilityOff } from "@mui/icons-material";
 import Copyright from "../utils/Copyright";
-import { loginSchema } from "../utils/schemas";
 
 
 
@@ -16,64 +12,59 @@ const Login = () => {
     const navigate = useNavigate();
     const [isShowPassword, setIsShowPassword] = useState(false);
     const { isLoggedIn } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-        validationSchema: loginSchema,
-        onSubmit: async (values) => {
-            try {
-                // Mengatur status loading menjadi true
-                setLoading(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-                // Melakukan dispatch login
-                await dispatch(login(values)).unwrap();
-
-                // Navigasi ke halaman dashboard setelah login berhasil
-                navigate("/dashboard");
+    const handleLogin = async () => {
+        try {
+            const response = await axiosInstance.post('/api/auth/signin', {
+                email,
+                password,
+            });
+        
+            // Menyimpan token ke dalam lokal storage
+            localStorage.setItem('accessToken', response.data.accessToken);
+        
+            // Lakukan sesuatu setelah login berhasil, misalnya navigasi ke halaman lain
+            console.log(response.data);
+            navigate("/dashboard");
             } catch (error) {
-                // Menangani error, misalnya menampilkan pesan kesalahan kepada pengguna
-                console.error(error);
-            } finally {
-                // Mengatur status loading menjadi false setelah selesai
-                setLoading(false);
+            console.error(error);
+            // Handle kesalahan, misalnya menampilkan pesan kesalahan kepada pengguna
             }
-        },
-    });
+        }
+
+    // const formik = useFormik({
+    //     initialValues: {
+    //         email: '',
+    //         password: '',
+    //     },
+    //     validationSchema: loginSchema,
+    //     onSubmit: async (values) => {
+    //         try {
+    //             // Mengatur status loading menjadi true
+    //             setLoading(true);
+
+    //             // Melakukan dispatch login
+    //             await dispatch(login(values)).unwrap();
+
+                
+    //         } catch (error) {
+    //             // Menangani error, misalnya menampilkan pesan kesalahan kepada pengguna
+    //             console.error(error);
+    //         } finally {
+    //             // Navigasi ke halaman dashboard setelah login berhasil
+    //             navigate("/dashboard");
+    //             // Mengatur status loading menjadi false setelah selesai
+    //             setLoading(false);
+    //         }
+    //     },
+    // });
 
     if (isLoggedIn) {
         return <Navigate to="/dashboard" />;
     }
-
-// import React, { useState } from 'react';
-// import axiosInstance from '../config/axiosInstance';
-
-// const Login = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   let navigate = useNavigate();
-
-//   const handleLogin = async () => {
-//     try {
-//       const response = await axiosInstance.post('/api/auth/signin', {
-//         email,
-//         password,
-//       });
-
-//       // Menyimpan token ke dalam lokal storage
-//       localStorage.setItem('accessToken', response.data.accessToken);
-
-//       // Lakukan sesuatu setelah login berhasil, misalnya navigasi ke halaman lain
-//       console.log(response.data);
-//       navigate("/dashboard");
-//     } catch (error) {
-//       console.error(error);
-//       // Handle kesalahan, misalnya menampilkan pesan kesalahan kepada pengguna
-//     }
-//   };
 
     return (
         <div className="main"> 
@@ -86,30 +77,18 @@ const Login = () => {
                     <Typography component={"h1"} variant="h5" color="ActiveCaption">
                         Sign In
                     </Typography>
-                        <form onSubmit={formik.handleSubmit} style={{alignItems: "center", margin: 2 }}>
                             <TextField 
+                                type="text" 
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)}
                                 fullWidth
-                                id="email"
-                                name="email"
-                                label="Email"
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                error={formik.touched.email && Boolean(formik.errors.email)}
-                                helperText={formik.touched.email && formik.errors.email}
                                 sx={{margin: 2}}
                             />
                             <div style={{ display: "flex", width: "100%", position: "relative" }}>
                                 <TextField 
                                     fullWidth
-                                    id="password"
-                                    name="password"
-                                    label="Password"
-                                    value={formik.values.password}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    error={formik.touched.password && Boolean(formik.errors.password)}
-                                    helperText={formik.touched.password && formik.errors.password}
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                     type={isShowPassword ? "text" : "password"} 
                                     sx={{marginTop: 2, marginLeft: 2, marginBottom: 2}}
                                 />
@@ -123,20 +102,15 @@ const Login = () => {
                                     )}
                                 </div>
                             </div>
-                            <Button fullWidth type="submit" variant="contained" disabled={loading} sx={{margin: 2}}>Sign In</Button>
-                        </form>
-                        {/* <div>
-                            <h1>Login</h1>
-                            <div>
-                                <label>Email:</label>
-                                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </div>
-                            <div>
-                                <label>Password:</label>
-                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                            </div>
-                            <button onClick={handleLogin}>Login</button>
-                        </div> */}
+                            <Button 
+                                fullWidth 
+                                type="submit" 
+                                onClick={handleLogin} 
+                                variant="contained" 
+                                disabled={loading} 
+                                sx={{margin: 2}}>
+                                    Sign In
+                            </Button>
                         <Copyright 
                             variant="body2"
                             color="ActiveCaption"
